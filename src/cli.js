374112d05a06297d5bb25f8084df3913924a16a7
Collen Jones <collen@uber.com>
2014-10-02 12:01:35 -0700

    Fix path / problem

--- src/cli.js
+++ src/cli.js
@@ -3,10 +3,11 @@
 var _ = require('underscore');
 var args = require('minimist')(process.argv.slice(2));
 var glob = require('glob-all');
+var path = require('path');
 var Human = require('./human');
 
 var directory = _.first(args._) || './';
-var allJSFiles = directory + '**/*.js';
+var allJSFiles = path.join(directory, '**/*.js');
 var excludedPattern = '!' + args.exclude;
 
 glob([allJSFiles, excludedPattern], {}, function(error, files){
