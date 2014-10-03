725ba7e2d6a895fc6f6e10292d8c1155375303b3
Willyham <willyd@uber.com>
2014-10-02 23:12:47 -0700

    Check file or dir

--- src/cli.js
+++ src/cli.js
@@ -4,11 +4,21 @@ var _ = require('underscore');
 var args = require('minimist')(process.argv.slice(2));
 var glob = require('glob-all');
 var path = require('path');
+var fs = require('fs');
 var Human = require('./human');
 
 var directory = _.first(args._) || './';
-var allJSFiles = path.join(directory, '**/*.js');
+var allJSFiles = getFileGlobString(directory);
 var excludedPattern = '!' + args.exclude;
+
+function getFileGlobString(arg){
+  var stat = fs.lstatSync(arg);
+  if(stat.isDirectory()){
+    return path.join(arg, '**/*.js');
+  }
+  return arg;
+}
+
 
 glob([allJSFiles, excludedPattern], {}, function(error, files){
   var human = new Human({
