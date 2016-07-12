d0b67df6baa87901868d0ffdfec8d2f48eb26645
Willyham <willyd@uber.com>
2016-07-12 16:52:39 +0100

    Add export, fix names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import noLoops from './rules/no-loops';
+import noElse from './rules/no-loops';
 
 const rules = {
-  'no-loops': noLoops
+  'human/no-loops': noLoops,
+  'human/no-else': noElse
 };
 
 export default rules;
